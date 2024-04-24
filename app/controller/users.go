31e48b8f0390b19bbe39dfaeff9c6be21c803fbf
Анастасия Грибович <boletusg8@gmail.com>
2024-04-24 23:24:32 +0700

    сохранение

--- app/controller/users.go
+++ app/controller/users.go
@@ -1,37 +1,35 @@
 package controller
 
-import (
-	"bookbuzz/app/model"
-	"encoding/json"
-	"github.com/julienschmidt/httprouter"
-	"html/template"
-	"net/http"
-	"path/filepath"
-)
-
+/*
 func GetUsers(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	//получаем список всех пользователей
+	// получаем список всех пользователей
 	users, err := model.GetAllUsers()
 	if err != nil {
-		http.Error(rw, err.Error(), 400)
-		return
-	}
-	//указываем путь к файлу с шаблоном
-	main := filepath.Join("public", "html", "usersDynamicPage.html")
-	//создаем html-шаблон
-	tmpl, err := template.ParseFiles(main)
-	if err != nil {
-		http.Error(rw, err.Error(), 400)
-		return
-	}
-	//исполняем именованный шаблон "users", передавая туда массив со списком пользователей
-	err = tmpl.ExecuteTemplate(rw, "users", users)
-	if err != nil {
-		http.Error(rw, err.Error(), 400)
+		log.Println("Ошибка при получении списка пользователей:", err)
+		http.Error(rw, "Ошибка при получении списка пользователей", http.StatusInternalServerError)
 		return
 	}
 
+	// указываем путь к файлу с шаблоном
+	main := filepath.Join("public", "html", "usersDynamicPage.html")
+	// создаем html-шаблон
+	tmpl, err := template.ParseFiles(main)
+	if err != nil {
+		log.Println("Ошибка при загрузке HTML-шаблона:", err)
+		http.Error(rw, "Ошибка при загрузке HTML-шаблона", http.StatusInternalServerError)
+		return
+	}
+
+	// исполняем именованный шаблон "users", передавая туда массив со списком пользователей
+	err = tmpl.ExecuteTemplate(rw, "users", users)
+	if err != nil {
+		log.Println("Ошибка при выполнении шаблона:", err)
+		http.Error(rw, "Ошибка при выполнении шаблона", http.StatusInternalServerError)
+		return
+	}
 }
+
+/*
 func AddUser(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	//получаем значение из параметра name, переданного в форме запроса
 	name := r.FormValue("name_user")
@@ -58,4 +56,4 @@ func AddUser(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		http.Error(rw, err.Error(), 400)
 		return
 	}
-}
+}*/
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.2
 
 require (
 	github.com/denisenkom/go-mssqldb v0.12.3
+	github.com/jackc/pgx/v4 v4.18.3
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/julienschmidt/httprouter v1.3.0
 )
@@ -11,5 +12,14 @@ require (
 require (
 	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/puddle v1.3.0 // indirect
+	golang.org/x/crypto v0.20.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
